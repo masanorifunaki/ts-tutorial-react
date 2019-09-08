6d9152300feaab045107049138ab9e515213850a
舟木正憲 <funakimfunaki@gmail.com>
2019-09-08 20:28:09 +0900

    Add Timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,70 @@
-import React, { SyntheticEvent } from 'react';
-import { Button, Card, Statistic } from 'semantic-ui-react';
+import React from 'react';
+import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
 
 import './App.css';
 
+const LIMIT = 60;
+
 interface AppState {
-  count: number;
+  timeLeft: number;
 }
 
 // 第一引数 Props 第二引数 Local State
 class App extends React.Component<{}, AppState> {
+  timerId?: NodeJS.Timer;
+
   constructor(props: {}) {
     super(props);
-    this.state = { count: 0 };
+    this.state = { timeLeft: LIMIT };
   }
 
-  increment = (e: SyntheticEvent) => {
-    e.preventDefault();
-    this.setState(prevState => ({
-      count: prevState.count + 1,
-    }));
+  reset = () => {
+    this.setState({ timeLeft: LIMIT });
   };
 
-  decrement = (e: SyntheticEvent) => {
-    e.preventDefault();
-    this.setState(prevState => ({
-      count: prevState.count - 1,
-    }));
+  tick = () => {
+    this.setState(prevState => ({ timeLeft: prevState.timeLeft - 1 }));
+  };
+
+  // コンポーネントがマウントされた直後に
+  // 呼ばれる
+  componentDidMount = () => {
+    this.timerId = setInterval(this.tick, 1000);
+  };
+
+  // コンポーネントが変更された直後に呼ば
+  // れる
+  componentDidUpdate = () => {
+    const { timeLeft } = this.state;
+    if (timeLeft === 0) {
+      this.reset();
+    }
+  };
+
+  // コンポーネントがアンマウントされる直
+  // 前に呼ばれる
+  componentWillUnmount = () => {
+    clearInterval(this.timerId as NodeJS.Timer);
   };
 
   render() {
-    const { count } = this.state;
+    const { timeLeft } = this.state;
 
     return (
       <div className="container">
         <header>
-          <h1>カウンター</h1>
+          <h1>タイマー</h1>
         </header>
         <Card>
           <Statistic className="number-board">
-            <Statistic.Label>count</Statistic.Label>
-            <Statistic.Value>{count}</Statistic.Value>
+            <Statistic.Label>time</Statistic.Label>
+            <Statistic.Value>{timeLeft}</Statistic.Value>
           </Statistic>
           <Card.Content>
-            <div className="ui two buttons">
-              <Button color="red" onClick={this.decrement}>
-                -1
-              </Button>
-              <Button color="green" onClick={this.increment}>
-                +1
-              </Button>
-            </div>
+            <Button color="red" onClick={this.reset}>
+              <Icon name="redo" />
+              Rest
+            </Button>
           </Card.Content>
         </Card>
       </div>
