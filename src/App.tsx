51e39d0d01e5040c0a6446e9ff310e0237212ad9
舟木正憲 <funakimfunaki@gmail.com>
2019-09-08 20:49:35 +0900

    Use functional components instead of class components

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,37 @@
-import React from 'react';
-import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
+import React, { FC } from 'react';
+import List, { Character } from './List';
 
 import './App.css';
 
-const LIMIT = 60;
+const App: FC<{}> = () => {
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: '羽咲 綾乃',
+      age: 16,
+      height: 151,
+    },
+    {
+      id: 2,
+      name: '荒垣 なぎさ',
+      age: 18,
+      height: 174,
+    },
+    {
+      id: 3,
+      name: '泉 理子',
+      age: 18,
+    },
+  ];
 
-interface AppState {
-  timeLeft: number;
-}
-
-// 第一引数 Props 第二引数 Local State
-class App extends React.Component<{}, AppState> {
-  timerId?: NodeJS.Timer;
-
-  constructor(props: {}) {
-    super(props);
-    this.state = { timeLeft: LIMIT };
-  }
-
-  reset = () => {
-    this.setState({ timeLeft: LIMIT });
-  };
-
-  tick = () => {
-    this.setState(prevState => ({ timeLeft: prevState.timeLeft - 1 }));
-  };
-
-  // コンポーネントがマウントされた直後に
-  // 呼ばれる
-  componentDidMount = () => {
-    this.timerId = setInterval(this.tick, 1000);
-  };
-
-  // コンポーネントが変更された直後に呼ば
-  // れる
-  componentDidUpdate = () => {
-    const { timeLeft } = this.state;
-    if (timeLeft === 0) {
-      this.reset();
-    }
-  };
-
-  // コンポーネントがアンマウントされる直
-  // 前に呼ばれる
-  componentWillUnmount = () => {
-    clearInterval(this.timerId as NodeJS.Timer);
-  };
-
-  render() {
-    const { timeLeft } = this.state;
-
-    return (
-      <div className="container">
-        <header>
-          <h1>タイマー</h1>
-        </header>
-        <Card>
-          <Statistic className="number-board">
-            <Statistic.Label>time</Statistic.Label>
-            <Statistic.Value>{timeLeft}</Statistic.Value>
-          </Statistic>
-          <Card.Content>
-            <Button color="red" onClick={this.reset}>
-              <Icon name="redo" />
-              Rest
-            </Button>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <header>
+        <h1>はねバド！ キャラクター一覧</h1>
+      </header>
+      <List school="北小町高校" characters={characters} />
+    </div>
+  );
+};
 
 export default App;
